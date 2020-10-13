6c1975c8205fcb9fcb955d3e5d76194c8dec37d7
Rino111 <rinoproduction99@gmail.com>
2020-10-13 13:55:16 +0700

    01-02/tanah.py

---
+++ novice/01-02/tanah.py
@@ -0,0 +1,27 @@
+def tambah (a,b):
+    print(a,'+',b,'=',a+b)
+    print('penambahan')
+
+def perkalian(a,b):
+    print('perkalian')
+    return a * b
+
+def BubbleSort(val):
+    for namber in range(len(val)-1,0,-1):
+        for i in range(namber):
+            if val[i]>val[i+1]:
+                temp = val[i]
+                val[i] = val[i+1]
+                val[i+1] = temp
+
+def SelectionSort(val):
+   for isi in range(len(val)-1,0,-1):
+       Max=0
+       for lokasi in range(1,isi+1):
+           if val[lokasi]>val[Max]:
+               Max = lokasi
+ 
+       temp = val[isi]
+       val[isi] = val[Max]
+       val[Max] = temp
+ 
