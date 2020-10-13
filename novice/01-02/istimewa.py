867e299c4f3a1c22b5f8f58d2f03f939719a74bb
Rino111 <rinoproduction99@gmail.com>
2020-10-13 13:53:44 +0700

    01-02/istimewa.py

---
+++ novice/01-02/istimewa.py
@@ -0,0 +1,14 @@
+
+import tanah as jumlah
+
+jumlah.tambah(6,5)
+perkalian = jumlah.perkalian(9,5)
+print(perkalian)
+
+DaftarAngka = [50,9,62,99,2,35,41,80]
+BubbleSort = jumlah.BubbleSort(DaftarAngka)
+print(DaftarAngka)
+
+angka = [53,24,83,43,44,21,91,90]
+SelectionSort = jumlah.SelectionSort(angka)
+print(angka)
