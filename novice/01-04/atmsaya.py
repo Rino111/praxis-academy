af190dbc55d7eb24031a31b3bd0d79e8663bb5b0
Rino111 <rinoproduction99@gmail.com>
2020-10-16 14:57:32 +0700

    uml

---
+++ novice/01-04/atmsaya.py
@@ -0,0 +1,32 @@
+print("Selamat datang di ATM saya")
+print("Pilih Option")
+print("1. Check uang saya")
+print("2. Ambil uang saya")
+print("3. Tabung Uang saya")
+Option=int(input("Silahkan Pilih Option :"))
+if Option==1:
+    print("Uang kamu berjumlah 200.000")
+elif Option==2:
+    print("Uang kamu Berjumlah Rp.200.000, Mau ambil berapa")
+    print("1. Rp.100.000")
+    print("2.Rp.200.000")
+    uang_kamu=500000
+    option=int(input("Option :"))
+    if option==1
+        hasil=uang_kamu-100000
+        print("Uang kamu sekarang berjumlah:",hasil1)
+    elif option2==2:
+        hasil2==uang_kamu-200000
+        print("Uang kamu sekarang berjumlah :",hasil2)
+
+    else:
+        print("Keyword Anda Salah")
+elif option==3:
+    uang_kamu=200000
+    print("Uang berjumlah Rp200.000, Mau nabung berapa ?")
+    option3==int(input("Masukan jumlah uang :"))
+    hasil3=uang_kamu+option3
+    print("Jumlah uang kamu sekarang adalah ",hasil3)
+else:
+    print("Keyword anda salah, mohon coba")
+
